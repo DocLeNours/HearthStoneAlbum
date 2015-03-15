c610635937b6e0d1c42890cd6ad2be17d284b57b
DocLeNours <DocLeNours@outlook.fr>
2015-03-15 17:52:07 +0100

    Add sln Add Domain.Core Add Album and Card classes

---
+++ HearthStoneAlbum.Domain.Core/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthStoneAlbum.Domain.Core
+{
+    public class Album
+    {
+        public int UserId { get; private set; }
+    }
+}
